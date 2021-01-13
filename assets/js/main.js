0581804a7bb2367da75d28010da6d0fed5147cbe
ghozziayoub <ghozziayoub@gmail.com>
2021-01-14 00:03:43 +0100

    working on best practicies

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,11 +181,11 @@
   });*/
 
   // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
+  /*$(".portfolio-details-carousel").owlCarousel({
     autoplay: true,
     dots: true,
     loop: true,
     items: 1
-  });
+  });*/
 
 })(jQuery);
